56a96c647aa406e7b897f74732ba5213a5b4f95f
ShiakiFromSchool <j2321310@okinawa-pc.ac.jp>
2025-01-07 16:18:16 +0900

    Making monitoring page. It's going well but , looks boring.

--- Syasyutsu/app/Livewire/PLCMonitor.php
+++ Syasyutsu/app/Livewire/PLCMonitor.php
@@ -7,9 +7,26 @@ use Livewire\Component;
 class PLCMonitor extends Component
 {
     public $count = 1;
-    public $data = "";
 
-    public function increment()
+    /*成形機　データ */
+    public $seikeiki_data = "";
+    public $seikeiki_error = "";
+
+    /*アームロボット　データ */
+    public $arm_data = "";
+    public $arm_error = "";
+
+    /*ゲートカット　データ */
+    public $gate_data = "";
+    public $gate_error = "";
+
+    /*画像処理用端末　データ */
+    public $CVA_data = "";
+    public $CVB_data = "";
+    public $seikeihin_good = "";
+    public $seikeihin_error = "";
+
+    public function monitoring()
     {
         // $this->count++;
 
@@ -36,6 +53,30 @@ class PLCMonitor extends Component
 		// }
 
         // $this->data = $fixed[1];
+    }
+
+    public function test_monitoring(){
+        $output = [];
+
+        exec("python3 ./python/hello2.py",$output);   //Pythonからテストメッセージを取得
+
+            /*成形機　データ */
+    $this->seikeiki_data = $output[0];
+    $this->seikeiki_error = $output[1];
+
+    /*アームロボット　データ */
+    $this->arm_data = $output[2];
+    $this->arm_error = $output[3];
+
+    /*ゲートカット　データ */
+    $this->gate_data = $output[4];
+    $this->gate_error = $output[5];
+
+    /*画像処理用端末　データ */
+    $this->CVA_data = $output[6];
+    $this->CVB_data = $output[7];
+    $this->seikeihin_good = $output[8];
+    $this->seikeihin_error = $output[9];
     }
 
     public function exec_python(){
--- Syasyutsu/resources/views/livewire/p-l-c-monitor.blade.php
+++ Syasyutsu/resources/views/livewire/p-l-c-monitor.blade.php
@@ -7,7 +7,7 @@
             </div>
             <div class="w3-display-right">
                     <a href="/" style="text-decoration:none;">
-                        <p class="w3-sans-serif">LOGO&emsp;</p>
+                        <p class="w3-sans-serif">LOGO</p>
                     </a>
             </div>
         </div>
@@ -16,44 +16,67 @@
     <div class="main">
         <div class="base-content w3-display-container">
             <div class="w3-display-middle">
-                <table border="1" class="monitor">
+                <table border="1" class="base-table">
                     <tr>
-                        <td>
-                            <table>
-                                <tr>
-                                    <th colspan="2">成形機</th>
+                        <th class="base-table-headder">成形機</th>
+                        <th class="base-table-headder">アームロボット</th>
+                        <th class="base-table-headder">ゲートカット装置</th>
+                        <th class="base-table-headder">寸法測定システム</th>
+                    </tr>
+                    <tr>
+                        <td class="base-table-data">
+                            <table border="1" class="monitor-table">
+                                <tr class="monitor-table-row">
+                                    <th class="monitor-table-headder">状態</th>
+                                    <td class="monitor-table-data">{{ $seikeiki_data }}</td>
                                 </tr>
-                                <tr>
-                                    <th>状態：</th>
-                                    <td>生成中</td>
-                                </tr>
-                            </table>
-                        </td>
-
-                        <td>
-                            <table>
-                                <tr>
-                                    <th colspan="2">ゲートカット</th>
-                                </tr>
-                                <tr>
-                                    <th>状態：</th>
-                                    <td>カット中</td>
+                                <tr class="monitor-table-row">
+                                    <th class="monitor-table-headder">エラー数</th>
+                                    <td class="monitor-table-data">{{ $seikeiki_error }}</td>
                                 </tr>
                             </table>
                         </td>
-
-                        <td>
-                        <table>
-                                <tr>
-                                    <th colspan="2">寸法測定システム</th>
+                        <td class="base-table-data">
+                            <table border="1" class="monitor-table">
+                                <tr class="monitor-table-row">
+                                    <th class="monitor-table-headder">状態</th>
+                                    <td class="monitor-table-data">{{ $arm_data }}</td>
                                 </tr>
-                                <tr>
-                                    <th>画像処理用端末A：</th>
-                                    <td>測定中</td>
+                                <tr class="monitor-table-row">
+                                    <th class="monitor-table-headder">エラー数</th>
+                                    <td class="monitor-table-data">{{ $arm_error }}</td>
                                 </tr>
-                                <tr>
-                                    <th>画像処理用端末B：</th>
-                                    <td>待機中</td>
+                            </table>
+                        </td>
+                        <td class="base-table-data">
+                            <table border="1" class="monitor-table">
+                                <tr class="monitor-table-row">
+                                    <th class="monitor-table-headder">状態</th>
+                                    <td class="monitor-table-data">{{ $gate_data }}</td>
+                                </tr>
+                                <tr class="monitor-table-row">
+                                    <th class="monitor-table-headder">エラー数</th>
+                                    <td class="monitor-table-data">{{ $gate_error }}</td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td class="base-table-data">
+                            <table border="1" class="monitor-table">
+                                <tr class="monitor-table-row">
+                                    <th class="monitor-table-headder">画像処理用端末A</th>
+                                    <td class="monitor-table-data">{{ $CVA_data }}</td>
+                                </tr>
+                                <tr class="monitor-table-row">
+                                    <th class="monitor-table-headder">画像処理用端末B</th>
+                                    <td class="monitor-table-data">{{ $CVB_data }}</td>
+                                </tr>
+                                <tr class="monitor-table-row">
+                                    <th class="monitor-table-headder">良品数</th>
+                                    <td class="monitor-table-data">{{ $seikeihin_good }}</td>
+                                </tr>
+                                <tr class="monitor-table-row">
+                                    <th class="monitor-table-headder">不良数</th>
+                                    <td class="monitor-table-data">{{ $seikeihin_error }}</td>
                                 </tr>
                             </table>
                         </td>
@@ -66,7 +89,7 @@
     <div class="footer">
         <div class="content2 w3-display-container">
             <div class="w3-display-bottomright">
-                <p>作成日　2024/12/4(水)&emsp;</p>
+                <p>作成日　2024/12/4(水)</p>
             </div>
         </div>
     </div>
@@ -75,9 +98,9 @@
     @script
     <script>
         setInterval(() => {
-            //Livewire内部で動作しているオブジェクトのメソッドを50000msごとに実行
-            $wire.increment()
-        }, 50000)
+            //Livewire内部で動作しているオブジェクトのメソッドを500msごとに実行
+            $wire.test_monitoring()
+        }, 500)
     </script>
     @endscript
 </div>
